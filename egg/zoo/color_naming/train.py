21ecd0e643395c3d40ba7e29053e5f7d51858ec9
rahmacha <chaabounirahma@gmail.com>
2019-08-27 08:34:34 -0700

    monitor H and IM during training

--- egg/zoo/color_naming/train.py
+++ egg/zoo/color_naming/train.py
@@ -10,6 +10,8 @@ from torch.nn import functional as F
 import torch.distributions
 import egg.core as core
 import argparse
+import numpy as np
+from sklearn.metrics import mutual_info_score
 
 import egg.core as core
 from egg.zoo.color_naming.data import ColorData, build_distance_matrix
@@ -33,6 +35,26 @@ def get_params(params):
     args = core.init(arg_parser=parser, params=params)
     return args
 
+def compute_Entropy(variables):
+    dico = {}
+    for variable in variables:
+        m = variable.argmax().detach().item()
+        if m in dico.keys():
+            dico[m] += 1.
+        else:
+            dico[m] = 1.
+    #freq_dico = {}
+    entropy = 0.
+    for key, value in dico.items():
+        tmp = value/sum([*dico.values()])
+        #freq_dico[key] = tmp
+        entropy += -tmp*np.log(tmp)
+    return entropy
+
+def compute_MI(variables1, variables2):
+    return mutual_info_score(variables1, variables2)
+
+
 class CylinderL1Loss(nn.Module):
     def __init__(self, distance_matrix):
         super().__init__()
@@ -43,8 +65,16 @@ class CylinderL1Loss(nn.Module):
         color_ids = sender_input[:, 0]
         distances = self.distance_matrix[color_ids].unsqueeze(1)
         expectation = torch.bmm(distances, receiver_output.unsqueeze(-1)).squeeze()
-        acc = (sum(receiver_output.argmax(dim=1)==sender_input[:, 0]).float()/len(sender_input[:, 0])).detach().item()
-        return expectation, {'acc': acc}
+        acc = (receiver_output.argmax(dim=1)== color_ids).float()
+        # Compute H(messages) and I(messages, inputs) and I(messages, outputs)
+        H_m = compute_Entropy(_message)
+        messages = []
+        for m in _message:
+            messages.append(m.argmax().detach().item())
+        I_m_inp = compute_MI(messages, color_ids.detach().tolist())
+        I_m_out = compute_MI(messages, receiver_output.detach().argmax(dim=1).tolist())
+
+        return expectation, {'acc': acc, 'Hm': H_m, 'I_m_inp': I_m_inp, 'I_m_out': I_m_out}
 
 def cross_entropy(sender_input, _message, _receiver_input, receiver_output, _labels):
     receiver_output = receiver_output.squeeze(1)
@@ -54,19 +84,20 @@ def cross_entropy(sender_input, _message, _receiver_input, receiver_output, _lab
     return loss, {'acc': acc}
 
 def dump(game, loader, device, gs_mode='gs'):
-    dataset = [x for x in loader][0][0].to(device)
-    messages = game.sender(dataset)
-    receiver_outputs = game.receiver(messages, None)
+    game.eval()
+    with torch.no_grad():
+        dataset = [x for x in loader][0][0].to(device)
+        messages = game.sender(dataset)
+        receiver_outputs = game.receiver(messages, None)
 
-    unif_acc = 0.
-    for (input_sender,message, output) in zip(dataset, messages, receiver_outputs):
-        inp = input_sender[0]
-        out = output.argmax(dim=0)
-        acc = inp==out
-        unif_acc+=acc
-        print(f'input: {inp.item()} -> message: {message.argmax().item()} -> output: {out.item()}', flush=True)
-
-    print(f'acc={unif_acc/dataset.size(0)}')
+        unif_acc = 0.0
+        for (input_sender,message, output) in zip(dataset, messages, receiver_outputs):
+            inp = input_sender[0]
+            out = output.argmax(dim=0)
+            acc = inp==out
+            unif_acc+=acc.item()
+            print(f'input: {inp.item()} -> message: {message.argmax().item()} -> output: {out.item()}', flush=True)
+        print(f'acc={unif_acc/dataset.size(0)}')
 
 def main(params):
     opts = get_params(params)
@@ -75,7 +106,7 @@ def main(params):
     train_loader = torch.utils.data.DataLoader(
         ColorData(scaler=opts.scaler), batch_size=opts.batch_size, shuffle=True)
     test_loader = torch.utils.data.DataLoader(
-        ColorData(), batch_size=N_COLOR_IDS - 1, shuffle=False)
+        ColorData(), batch_size=N_COLOR_IDS, shuffle=False)
 
     distance_matrix = build_distance_matrix(test_loader.dataset)
     distance_matrix = torch.from_numpy(distance_matrix).cuda().float()
