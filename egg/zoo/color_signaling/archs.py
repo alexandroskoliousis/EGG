23ca041141f5d881eead34f26d4ba4f9ac5cca0d
rahmacha <chaabounirahma@gmail.com>
2019-11-05 13:52:09 -0800

    cleaning

--- egg/zoo/color_signaling/archs.py
+++ egg/zoo/color_signaling/archs.py
@@ -14,14 +14,12 @@ class Sender(nn.Module):
         super(Sender, self).__init__()
         self.fc1 = nn.Linear(3, 500) # LAB is a 3D space
         self.fc2 = nn.Linear(500, vocab_size)
-        #self.fc = nn.Linear(3, vocab_size)
 
     def forward(self, x):
         x = x[:, 1:] # not taking the ID but only the LAB system
         x = self.fc1(x)
         x = F.leaky_relu(x)
         x = self.fc2(x)
-        #x = self.fc(x)
         return x.log_softmax(dim=-1)
 
 
@@ -29,14 +27,52 @@ class Receiver(nn.Module):
     def __init__(self, hidden):
         super(Receiver, self).__init__()
         self.fc = nn.Linear(3, hidden)
-        #self.fc2 = nn.Linear(hidden, 3)
-        #self.fc_message = nn.Linear(hidden, 3)
 
 
     def forward(self, x, _input):
-        #x = self.fc_message(x)
         _input = _input[:,:,1:]  # not taking the ID but only the LAB system
         embedded_input = self.fc(_input)
-        #embedded_input = _input
+        energies = torch.matmul(embedded_input, torch.unsqueeze(x, dim=-1))
+        return energies.squeeze()
+
+class Sender2(nn.Module):
+    def __init__(self, vocab_size, n_colors=330, ids=False):
+        super(Sender2, self).__init__()
+        if ids:
+            self.emb = nn.Embedding(n_colors, 3)
+        self.ids = ids
+        self.fc1 = nn.Linear(3, 500) # LAB is a 3D space
+        self.fc2 = nn.Linear(500, vocab_size)
+
+    def forward(self, x):
+        if self.ids:
+            x = x[:, 0:1].long() # only color-id
+            x = self.emb(x)
+        else:
+            x = x[:, 1:] # not taking the ID but only the LAB system
+        x = self.fc1(x)
+        x = F.leaky_relu(x)
+        x = self.fc2(x)
+        return x.log_softmax(dim=-1)
+
+
+class Receiver2(nn.Module):
+    def __init__(self, hidden, n_colors=330, ids=False):
+        super(Receiver2, self).__init__()
+        if ids:
+            self.emb = nn.Embedding(n_colors, hidden)
+        else:
+            self.fc = nn.Linear(3, hidden)
+        self.ids = ids
+
+
+    def forward(self, x, _input):
+        if self.ids:
+            _input = _input[:, 0:1].long() # only color-id
+            embedded_input = self.emb(_input)
+        else:
+            _input = _input[:,:,1:]  # not taking the ID but only the LAB system
+            embedded_input = self.fc(_input)
+
         energies = torch.matmul(embedded_input, torch.unsqueeze(x, dim=-1))
         return energies.squeeze()
